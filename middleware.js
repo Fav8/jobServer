e9f3f2e828f806963e5b0751aea38322a8284e05
leonardo d'amico <leonardo.damico7597@gmail.com>
2024-01-08 17:01:32 +0100

    fix getauth

--- middleware.js
+++ middleware.js
@@ -3,7 +3,7 @@ import {admin} from './firebase-config.js';
 export async function DecodeTokenMiddleware(req, res, next) {
     try {
         let idToken = req.headers.authorization.split(' ')[1]
-        let decodedToken = await admin.getAuth().verifyIdToken(idToken)
+        let decodedToken = await admin.auth().verifyIdToken(idToken)
         if (!decodedToken) {
             res.status(401)
             res.send('Unauthorized')
